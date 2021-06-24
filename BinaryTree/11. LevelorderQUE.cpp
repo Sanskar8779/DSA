204b425b4eb077977a0733a2f44db16f90f25945
Sanskar <sanskar.shubham28@gmail.com>
2021-06-24 20:36:38 +0530

    Levelorder Traversal using Queue is added

---
+++ BinaryTree/11. LevelorderQUE.cpp
@@ -0,0 +1,50 @@
+//Level order Traversal using queue
+
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int key)
+    {
+        data = key;
+        left = right = NULL;
+    }
+};
+
+void Levelorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (q.empty() != true)
+    {
+        Node *temp = q.front();
+        cout << temp->data << " ";
+        q.pop();
+        if (temp->left != NULL)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right != NULL)
+        {
+            q.push(temp->right);
+        }
+    }
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    Levelorder(root);
+}
