453a95710728cc309b2d62b6a907050959589750
Sanskar <sanskar.shubham28@gmail.com>
2021-06-29 18:30:22 +0530

    Level Order Line by Line added

---
+++ BinaryTree/15. LevelOrderLBL.cpp
@@ -0,0 +1,50 @@
+//Level Order Traversal Line by Line
+
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *left, *right;
+    Node(int key){
+        data = key;
+        left = right = NULL;
+    }
+};
+void LevelOrderLinebyLine(Node *root){
+    if(root == NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(true){
+        int nodeCount = q.size();
+        if(nodeCount == 0){
+            break;
+        }
+        while(nodeCount > 0){
+            Node *temp = q.front();
+            cout<<temp->data<<" ";
+            q.pop();
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+            nodeCount--;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    LevelOrderLinebyLine(root);
+    return 0;
+}
